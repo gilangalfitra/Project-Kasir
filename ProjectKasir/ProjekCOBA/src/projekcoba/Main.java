477b1f6d9c2bdb3d2f707e8da2ec9ca6cbc8e93e
Gilang Alfitra <134423982+gilangalfitra@users.noreply.github.com>
2023-07-02 15:06:47 +0700

    Add files via upload

---
+++ ProjectKasir/ProjekCOBA/src/projekcoba/Main.java
@@ -0,0 +1,15 @@
+/*
+AUTHOR          : GILANG ALFITRA
+TANGGAL         : 17 Juni 2022
+TUJUAN PROGRAM  : GUI Java
+ */
+package projekcoba;
+
+public class Main {
+
+    public static void main(String[] args) {
+        halamanKasirr halKasir = new halamanKasirr();
+        halKasir.setVisible(true);
+    }
+    
+}
